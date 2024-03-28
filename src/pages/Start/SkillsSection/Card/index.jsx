13daa97df983fa9f046757d8267902fcad61b9cf
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2024-03-28 20:13:45 -0300

    Fazendo ajustes na seção de habilidades

--- src/pages/Start/SkillsSection/Card/index.jsx
+++ src/pages/Start/SkillsSection/Card/index.jsx
@@ -1,11 +1,11 @@
 export function Card({skill}) {
   return (
-    <div className='flex flex-col justify-center gap-4 items-center dark:text-light-200'>
+    <div className='flex flex-col justify-center gap-4 items-center dark:text-light-200 w-28'>
       <img src={skill.icon} alt={skill.name} className='aspect-square max-w-16 max-h-16 hover:-translate-y-2 duration-500' />
-      <p className='text-center font-developer font-semibold text-lg'>
+      <p className='text-center font-extrabold tracking-wider'>
         {skill.name}
       </p>
-      <p className='text-gray-700 dark:text-gray-400 font-medium'>
+      <p className='text-gray-500 dark:text-gray-500 font-medium text-sm'>
         {skill.level}
       </p>
     </div>
--- src/pages/Start/SkillsSection/index.jsx
+++ src/pages/Start/SkillsSection/index.jsx
@@ -5,8 +5,8 @@ import skills from '/src/data/skills.json';
 export function SkillsSection() {
   return (
     <SectionTemplate title="Habilidades">
-      <div className="flex justify-center px-2">
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6 gap-y-10 justify-center border-2 border-gray-400 dark:border-[#3f3f3f] rounded-xl px-4 md:px-6 py-10 sm:py-10 box-border max-w-2xl lg:max-w-4xl xl:max-w-5xl">
+      <div className="flex justify-center px-1 sm:px-0 py-3">
+        <div className="flex w-full flex-wrap gap-x-[4vw] gap-y-6 justify-center ">
           {skills.map((skill) => (
             <Card skill={skill} key={skill.name} />
           ))}
